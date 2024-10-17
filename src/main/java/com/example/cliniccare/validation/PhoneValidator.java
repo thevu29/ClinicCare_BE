851529c00d419d4335e7ca19c49cd992b0e9bfe3
thevu29 <vnguyen132az@gmail.com>
2024-10-17 07:42:17 +0700

    fix phone validator

--- src/main/java/com/example/cliniccare/validation/PhoneValidator.java
+++ src/main/java/com/example/cliniccare/validation/PhoneValidator.java
@@ -10,7 +10,7 @@ public class PhoneValidator implements ConstraintValidator<ValidPhone, String> {
             return true;
         }
 
-        return phone.matches("^(\\\\+84|0)(3[2-9]|7[0-9]|8[0-9]|9[0-9]|1[2-9]|5[0-9]|4[0-9])[0-9]{7}$");
+        return phone.matches("^(\\+84|0)(3[2-9]|7[0-9]|8[0-9]|9[0-9]|1[2-9]|5[0-9]|4[0-9])[0-9]{7}$");
     }
 }
 
