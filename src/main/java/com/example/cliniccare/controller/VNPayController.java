5e7c05b9752ec942f1e7d6a36e086bd2cdaa8394
thientranreal <thientt2612@gmail.com>
2024-11-16 10:48:10 +0700

    add vnpaycontroller

---
+++ src/main/java/com/example/cliniccare/controller/VNPayController.java
@@ -0,0 +1,106 @@
+package com.example.cliniccare.controller;
+
+import com.example.cliniccare.response.ApiResponse;
+import com.example.cliniccare.utils.VNPayUtils;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api/vnpay")
+public class VNPayController {
+//    Get value from env
+    @Value("${vnp_TmnCode}")
+    private String vnp_TmnCode;
+
+    @Value("${vnp_HashSecret}")
+    private String secretKey;
+
+    @Value("${vnp_PayUrl}")
+    private String vnp_PayUrl;
+
+    @Value("${vnp_ReturnUrl}")
+    private String vnp_ReturnUrl;
+
+//    private String vnp_ApiUrl = "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction";
+
+    @GetMapping("/create-payment")
+    public ResponseEntity<?> createPayment(
+            @RequestParam int amount,
+            HttpServletRequest req
+    ) throws IOException {
+        String vnp_Version = "2.1.0";
+        String vnp_Command = "pay";
+        String orderType = "other";
+        long amountFormat = amount * 100L;
+        String bankCode = "NCB";
+
+        String vnp_TxnRef = VNPayUtils.getRandomNumber(8);
+        String vnp_IpAddr = VNPayUtils.getIpAddress(req);
+
+        Map<String, String> vnp_Params = new HashMap<>();
+        vnp_Params.put("vnp_Version", vnp_Version);
+        vnp_Params.put("vnp_Command", vnp_Command);
+        vnp_Params.put("vnp_TmnCode", vnp_TmnCode);
+        vnp_Params.put("vnp_Amount", String.valueOf(amountFormat));
+        vnp_Params.put("vnp_CurrCode", "VND");
+
+        vnp_Params.put("vnp_BankCode", bankCode);
+        vnp_Params.put("vnp_TxnRef", vnp_TxnRef);
+        vnp_Params.put("vnp_OrderType", orderType);
+        vnp_Params.put("vnp_OrderInfo", "Thanh toan don hang:" + vnp_TxnRef);
+        vnp_Params.put("vnp_Locale", "vn");
+
+        vnp_Params.put("vnp_ReturnUrl", vnp_ReturnUrl);
+        vnp_Params.put("vnp_IpAddr", vnp_IpAddr);
+
+        Calendar cld = Calendar.getInstance(TimeZone.getTimeZone("Etc/GMT+7"));
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMddHHmmss");
+        String vnp_CreateDate = formatter.format(cld.getTime());
+        vnp_Params.put("vnp_CreateDate", vnp_CreateDate);
+
+        cld.add(Calendar.MINUTE, 15);
+        String vnp_ExpireDate = formatter.format(cld.getTime());
+        vnp_Params.put("vnp_ExpireDate", vnp_ExpireDate);
+
+        List fieldNames = new ArrayList(vnp_Params.keySet());
+        Collections.sort(fieldNames);
+        StringBuilder hashData = new StringBuilder();
+        StringBuilder query = new StringBuilder();
+        Iterator itr = fieldNames.iterator();
+        while (itr.hasNext()) {
+            String fieldName = (String) itr.next();
+            String fieldValue = (String) vnp_Params.get(fieldName);
+            if ((fieldValue != null) && (fieldValue.length() > 0)) {
+                //Build hash data
+                hashData.append(fieldName);
+                hashData.append('=');
+                hashData.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+                //Build query
+                query.append(URLEncoder.encode(fieldName, StandardCharsets.US_ASCII.toString()));
+                query.append('=');
+                query.append(URLEncoder.encode(fieldValue, StandardCharsets.US_ASCII.toString()));
+                if (itr.hasNext()) {
+                    query.append('&');
+                    hashData.append('&');
+                }
+            }
+        }
+        String queryUrl = query.toString();
+        String vnp_SecureHash = VNPayUtils.hmacSHA512(secretKey, hashData.toString());
+        queryUrl += "&vnp_SecureHash=" + vnp_SecureHash;
+        String paymentUrl = vnp_PayUrl + "?" + queryUrl;
+
+        return ResponseEntity.ok(new ApiResponse<>(
+                true, "Create payment successfully", paymentUrl
+        ));
+    }
+}
