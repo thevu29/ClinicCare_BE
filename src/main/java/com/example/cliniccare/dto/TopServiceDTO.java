320455cc2158bce66e0811477662ca05827bde19
thevu29 <vnguyen132az@gmail.com>
2024-12-04 22:38:54 +0700

    change dto

--- src/main/java/com/example/cliniccare/dto/TopServiceDTO.java
+++ src/main/java/com/example/cliniccare/dto/TopServiceDTO.java
@@ -12,12 +12,16 @@ public class TopServiceDTO {
     private UUID serviceId;
     private String name;
     private String image;
+    private double price;
+    private int discount;
     private long count;
 
     public TopServiceDTO(Service service, long count) {
         this.serviceId = service.getServiceId();
         this.name = service.getName();
         this.image = service.getImage();
+        this.price = service.getPrice();
+        this.discount = service.getPromotion() == null ? 0 : service.getPromotion().getDiscount();
         this.count = count;
     }
 }
